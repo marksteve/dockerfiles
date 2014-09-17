376657eec47420ccec6f053101b71343300623a5
Mark Steve Samson <hello@marksteve.com>
2014-09-17 12:28:30 +0800

    Move roBrowser to a separate docker image

--- rathena/run.sh
+++ rathena/run.sh
@@ -2,13 +2,9 @@
 docker run --rm \
   -ti \
   -v `pwd`/conf:/rathena/conf \
-  -v ~/ro/resources:/roBrowser/client/resources \
-  -v ~/ro/BGM:/roBrowser/client/BGM \
-  -v ~/ro/data:/roBrowser/client/data \
   -p 6900:6900 \
   -p 5121:5121 \
   -p 6121:6121 \
-  -p 8080:8080 \
   --link rathena-db:mysql \
   marksteve/rathena bash
 
